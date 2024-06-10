3f9a2d21de2ebb89550761eda7325ab2ae69f5e6
dan <md.erhenede@gmail.com>
2024-06-10 19:56:37 +0100

    removed secrets

--- client/src/assets/firebaseConfig.jsx
+++ client/src/assets/firebaseConfig.jsx
@@ -1,19 +1,26 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+const key = import.meta.env.VITE_FIREBASE_KEY;
+const domain = import.meta.env.VITE_FIREBASE_DOMAIN;
+const id = import.meta.env.VITE_FIREBASE_ID;
+const bucket = import.meta.env.VITE_FIREBASE_BUCKET;
+const senderId = import.meta.env.VITE_FIREBASE_SENDERID;
+const appId = import.meta.env.VITE_FIREBASE_APPID;
+const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENTID;
+
+console.log(key)
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA8dxpT8iiJMrImKJDGrg9ThgzCPa--rQ0",
-    authDomain: "social-media-fd6de.firebaseapp.com",
-    projectId: "social-media-fd6de",
-    storageBucket: "social-media-fd6de.appspot.com",
-    messagingSenderId: "714697265362",
-    appId: "1:714697265362:web:3bb6bdd3f1f7a55ccc5d0b",
-    measurementId: "G-CNMLCE1QY3"
+    apiKey: key,
+    authDomain: domain,
+    projectId: id,
+    storageBucket: bucket,
+    messagingSenderId: senderId,
+    appId: appId,
+    measurementId: measurementId
 };
 
 // Initialize Firebase
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
+const withMT = require("@material-tailwind/react/utils/withMT");
 export default withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-})
+});
